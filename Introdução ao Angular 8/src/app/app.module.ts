cabe2026ed6a469b8136f3f2438bf45a9ed2a889
RoAlencar <roalencarx@gmail.com>
2022-07-14 17:52:47 -0300

    Criando componentes com atributos interdependentes

--- Introdução ao Angular 8/src/app/app.module.ts
+++ Introdução ao Angular 8/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { StartComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule, 
