a6935ea92b6f621c3a41a909022ccdcf34026399
RoAlencar <roalencarx@gmail.com>
2022-07-18 19:44:17 -0300

    Tratando erros com Angular

--- Introdução ao Angular 8/src/app/app.module.ts
+++ Introdução ao Angular 8/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
@@ -12,7 +11,6 @@ import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule, 
@@ -22,8 +20,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
           {
         path : '', redirectTo: 'courses', pathMatch: 'full'
-      },{
-        path: '**', component: Error404Component
       }
     ])
   ],
