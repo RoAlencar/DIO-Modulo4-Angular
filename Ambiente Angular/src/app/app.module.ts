5dfb9f72c4e49f94963f531e51eeae0ec345cb4c
RoAlencar <roalencarx@gmail.com>
2022-07-22 17:39:18 -0300

    Refatorando projeto e Construindo um serviço com o comando ng g service

---
+++ Ambiente Angular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
+import { PeopleService } from './shared/services/people.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MarcaTextoDirective,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [PeopleService,],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
