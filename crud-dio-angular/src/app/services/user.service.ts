54c60e72df3a84724689033e191dc5e923b79dc3
RoAlencar <roalencarx@gmail.com>
2022-07-24 16:59:08 -0300

    Desenvolvendo o método para salvar o usuário no banco

--- crud-dio-angular/src/app/services/user.service.ts
+++ crud-dio-angular/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -9,9 +9,20 @@ import { User } from '../models/user';
 export class UserService {
 
   apiUrl='https://sheet.best/api/sheets/42417d7e-25c3-42dc-99fa-f67d11a66e5b'
+
+  httpOptions = {
+    headers : new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
+  }
+
   constructor(private httpClient : HttpClient) { }
 
   getUsers() : Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  postUser(user : User) : Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl,user, this.httpOptions);
   }
 }
